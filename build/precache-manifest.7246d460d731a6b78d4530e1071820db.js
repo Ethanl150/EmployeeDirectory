794b71681ef2ca468ab0863c90f94c9144083619
Ethan Lacy <ethanr0m818@gmail.com>
2020-04-03 17:36:54 -0400

    added build folder

---
+++ build/precache-manifest.7246d460d731a6b78d4530e1071820db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49fac795504f9cdd3d11ce25744fc66c",
+    "url": "/EmployeeDirectory/index.html"
+  },
+  {
+    "revision": "a466f89ebd3a4c7aa947",
+    "url": "/EmployeeDirectory/static/css/main.bed96799.chunk.css"
+  },
+  {
+    "revision": "3597e88a317bcc8f3dc5",
+    "url": "/EmployeeDirectory/static/js/2.7a47a45f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/EmployeeDirectory/static/js/2.7a47a45f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a466f89ebd3a4c7aa947",
+    "url": "/EmployeeDirectory/static/js/main.b48af15b.chunk.js"
+  },
+  {
+    "revision": "09754f8e7c235ddb6714",
+    "url": "/EmployeeDirectory/static/js/runtime-main.d78221c9.js"
+  }
+]);
